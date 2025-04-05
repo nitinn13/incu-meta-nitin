94ae6c37e0f396a40304829c4605b92f875ce3d2
Parth-1104 <141064865+Parth-1104@users.noreply.github.com>
2025-04-06 02:32:46 +0530

    Re-add videosdk-postcall-transcription-feature as a plain folder

---
+++ videosdkTranscript/videosdk-postcall-transcription-feature/src/API.js
@@ -0,0 +1,64 @@
+import { APP_URL } from './constants/constants';
+
+// unsafe Token
+export const authToken = import.meta.env.VITE_VIDEOSDK_TOKEN;
+
+// create Meeting
+export const createMeeting = async () => {
+  const res = await fetch(`${APP_URL}/v2/rooms`, {
+    method: 'POST',
+    headers: {
+      authorization: `${authToken}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      webhook: {
+        endPoint: 'https://webhook.site/a5301d95-c4b2-4679-ad09-8800f04231cb',
+        events: [
+          'recording-started',
+          'recording-stopped',
+          'transcription-started',
+          'transcription-stopped',
+        ],
+      },
+      autoStartConfig: {
+        recording: {
+          transcription: {
+            enabled: true,
+            summary: {
+              enabled: true,
+              prompt:
+                'Write summary in sections like Title, Agenda, Speakers, Action Items, Outlines, Notes and Summary',
+            },
+          },
+          config: {
+            layout: {
+              type: 'GRID',
+              priority: 'SPEAKER',
+              gridSize: 4,
+            },
+          },
+        },
+      },
+    }),
+  });
+  const { roomId } = await res.json();
+  return roomId;
+};
+
+// validating meeting
+export const validateMeeting = async (id) => {
+  const res = await fetch(`${APP_URL}/v2/rooms/validate/${id}`, {
+    method: 'GET',
+    headers: {
+      authorization: `${authToken}`,
+      'Content-Type': 'application/json',
+    },
+  });
+  if (res.ok) {
+    const { roomId } = await res.json();
+    return roomId;
+  } else {
+    alert('invalid meeting id');
+  }
+};
